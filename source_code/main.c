c66dfe8efc56757f4973cfd3cc8c42efbf20b043
Ledyxoo <77828064+Ledyxoo@users.noreply.github.com>
2023-10-09 13:08:49 +0200

    Update main.c

--- source_code/main.c
+++ source_code/main.c
@@ -1,154 +1,50 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "model.h"
-#include <stdbool.h>
 #include "SDL.h"
 #include "SDL2_gfxPrimitives.h"
-
-#define FPS             60
-#define FRAME_TIME      (1000 / FPS)
-
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-int          last_frame_time;
-float             delta_time;
-int init_window(Game* game)
-{
-
-    window = SDL_CreateWindow(
-        "Hello World!",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        game->universe->win_width,
-        game->universe->win_height,
-        SDL_WINDOW_SHOWN);
-
-    if (!window)
-    {
-        printf("Window creation failed\n");
-        return false;
-    }
-
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    if (!renderer)
-    {
-        printf("Renderer creation failed\n");
-        return false;
-    }
-
-    return true;
-}
-
-void setup(Game* game)
-{
-    game->spaceship->height = 10;
-    game->spaceship->width = 10;
-    game->spaceship->pos_x = game->universe->start->pos_x;
-    game->spaceship->pos_y = game->universe->start->pos_y;
-}
-
-void process_input(Game* game)
-{
-    SDL_Event e;
-    SDL_PollEvent(&e);
-
-    switch (e.type)
-    {
-    case SDL_QUIT:
-        game->state = GAME_IS_OVER;
-        break;
-    case SDL_KEYDOWN:
-        if (e.key.keysym.sym == SDLK_ESCAPE)
-            game->state = GAME_IS_OVER;
-        if (e.key.keysym.sym == SDLK_d)     // ->
-            game->spaceship->pos_x += (1 * delta_time);   //
-        if (e.key.keysym.sym == SDLK_q)     // <-
-            game->spaceship->pos_x -= (1 * delta_time);   // 
-        if (e.key.keysym.sym == SDLK_s)     // DOWN
-            game->spaceship->pos_y += (1 * delta_time);   //
-        if (e.key.keysym.sym == SDLK_z)     // UP
-            game->spaceship->pos_y -= (1 * delta_time);   //
-
-        break;
-    }
-}
-
-void update()
-{
-    // TODO: CAP FRAME RATE
-    // last_frame_time = SDL_GetTicks();
-
-    // int wait = FRAME_TIME - (SDL_GetTicks() - last_frame_time);
-
-    // if (wait > 0 && wait <= FRAME_TIME)
-    // {
-    //     SDL_Delay(wait);
-    // }
-
-    delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0;
-
-}
-
-void render(Game* game)
-{
-    // Background : BLACK
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    // Player : RED
-    SDL_Rect player = {
-        game->spaceship->pos_x,
-        game->spaceship->pos_y,
-        game->spaceship->width,
-        game->spaceship->height
-    };
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderFillRect(renderer, &player);
-
-
-
-    // Finish : White
-    SDL_Rect finish = {
-        game->universe->finish->pos_x,
-        game->universe->finish->pos_y,
-        game->universe->finish->width = 10,
-        game->universe->finish->height = 10
-    };
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, &finish);
-
-    for (int i = 0; i < game->universe->nb_solar_systems; i++)
-    {
-        filledCircleRGBA(renderer, game->universe->solar_systems[i].sun.pos_x, game->universe->solar_systems[i].sun.pos_y, game->universe->solar_systems[i].sun.radius, 255, 215, 0, 255);
-        for (int j = 0; j < game->universe->solar_systems[i].nb_planets; j++)
-        {
-            int orbit_radius = abs(game->universe->solar_systems[i].planets[j].orbit_radius); // Prend la valeur absolue du rayon de l'orbite
-            filledCircleRGBA(renderer, game->universe->solar_systems[i].sun.pos_x, game->universe->solar_systems[i].sun.pos_y - orbit_radius, game->universe->solar_systems[i].planets[j].radius_mass_orbital_period, 0, 10, 255, 255);
-        }
-    }
-    SDL_RenderPresent(renderer);
-}
+#include "vue_controller.h"
+#include <time.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
+    int start = 0;
+    int x = 0;
+    srand(time(NULL));
+    x = rand () % 2;
 	Game* game = Game_New();
-
-	while (Game_GetState(game) == GAME_IN_PROGRESS)
-	{
-        SDL_Init(SDL_INIT_VIDEO);
-        init_window(game);
-        setup(game);
-        while (Game_GetState(game) == GAME_IN_PROGRESS)
+    SDL_Init(SDL_INIT_VIDEO);
+    init_window(game);
+    setup(game);
+    bool random_spaceship_initialized = false;
+    while (Game_GetState(game) == GAME_IN_PROGRESS)
+    {
+        process_input(game);
+        update(game);
+        render(game);
+        Game_UpdateState(game);
+        if (game->update_planets_start && !random_spaceship_initialized)
         {
-            process_input(game);
-            update();
-            render(game);
-            //printf("%d %d\n", game->spaceship->pos_x, game->spaceship->pos_y);
-            Game_UpdateState(game);
+            random_spaceship_angle(game->spaceship);
+            random_spaceship_initialized = true;
         }
-        SDL_DestroyWindow(window);
-        SDL_DestroyRenderer(renderer);
-        SDL_Quit();
-	}
+        if (game->update_planets_start)
+        {
+            if (x == 0)
+            {
+                game->spaceship->pos_x += game->spaceship->velocity * cos(game->spaceship->angle);
+                game->spaceship->pos_y -= game->spaceship->velocity * sin(game->spaceship->angle);
+            }
+            else
+            {
+                game->spaceship->pos_x -= game->spaceship->velocity * cos(game->spaceship->angle);
+                game->spaceship->pos_y -= game->spaceship->velocity * sin(game->spaceship->angle);
+            }
+           
+        }
+    }
+    free_universe(game->universe);
+    destroy_window();
     return 0;
 }
